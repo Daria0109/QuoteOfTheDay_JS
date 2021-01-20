ff7d38639f075bfc0e4e49117d81f6d2537a147d
Daria0109 <da6ulya888@gmail.com>
2021-01-20 11:59:24 +0300

    Actions block added

--- js/main.js
+++ js/main.js
@@ -1,20 +1,30 @@
-let viewedQuotes = [];
-
 const quote = document.querySelector(".quote");
 const author = document.querySelector(".author");
 const generateBtn = document.querySelector(".generate-btn");
 const loader = document.querySelector(".loader");
 const titleBlock = document.querySelector(".title-block");
-const container = document.querySelector(".container");
+const quoteContainer = document.querySelector(".quote-container");
 const quotePhoto = document.querySelector(".photo");
 
+const likesIcon = document.querySelector(".likes-icon");
+const likesSpan = document.querySelector(".likes-span");
+const shareIcon = document.querySelector(".share-icon");
+
 generateBtn.addEventListener("click", () => {
-  hideTitle();
-  container.classList.add("hide");
+  // hide a title
+  titleBlock.classList.add("hide");
+  // hide quote
+  quoteContainer.classList.add("hide");
+  // show loader
   loader.classList.toggle("active");
+  // generate quote
+  generateRandomQuote();
+});
+
+function generateRandomQuote() {
   setTimeout(() => {
     loader.classList.toggle("active");
-    container.classList.remove("hide");
+    quoteContainer.classList.remove("hide");
     if (!quotes.length) {
       quote.innerText = "Thanks, you've watched all quotes. Have a nice day!";
       author.innerText = "";
@@ -24,14 +34,15 @@ generateBtn.addEventListener("click", () => {
       quote.innerText = quotes[quoteId].quote;
       author.innerText = quotes[quoteId].author;
       quotePhoto.src = quotes[quoteId].photo;
+      likesIcon.style = "color: #fff";
       quotes.splice(quoteId, 1);
     }
   }, 2000);
-});
-
-function hideTitle() {
-  titleBlock.classList.add("hide");
 }
 function generateQuoteId() {
   return Math.floor(Math.random() * quotes.length);
 }
+
+likesIcon.addEventListener("click", () => {
+  likesIcon.style = "color: red";
+});
